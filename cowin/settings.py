926d9d7ab7cc438dfd2ac0c346a0980e49605a56
Abhijith Sathyendran <abhijith.sathyendran.com>
2021-05-09 23:03:21 +0530

    test

--- cowin/settings.py
+++ cowin/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,4 +145,4 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
-
+django_heroku.settings(locals())
